57b6db64959363a6351f3933c6568f62d67cc79b
salman-khan <salman.khan@consultancyoutfit.co.uk>
2025-07-29 14:28:46 +0500

    feat:about-us UI conmpleted

--- src/features/about-us/index.tsx
+++ src/features/about-us/index.tsx
@@ -1,15 +1,30 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import { Hero } from "./components";
+import TrustedBy from "./components/trusted-by";
+import ComplianceTrust from "./components/compliance-trust";
+import WhatWeDo from "./components/what-we-do";
+import BuildDeveloper from "./components/built-developer";
+import OurLeadership from "./components/our-leadership";
+import Products from "./components/products";
+import ContactUs from "./components/contact-us";
 export const AboutUsFeature = () => {
   return (
     <>
       <Box
         sx={{
-          backgroundColor: "common.light_background",
+          backgroundColor: "common.white",
         }}
       >
-        <Typography>About Us</Typography>
+        <Hero />
+        <TrustedBy />
+        <ComplianceTrust />
+        <WhatWeDo />
+        <BuildDeveloper />
+        <OurLeadership />
+        <Products />
+        <ContactUs />
       </Box>
     </>
   );
